503ef8d7a70bb964175a3af651d683d988f97fb2
Misha <misha.n@thewhitelabelagency.com>
2024-06-05 11:48:43 +0300

    task 3 - done

--- js/03-promises.js
+++ js/03-promises.js
@@ -1,46 +1,42 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-        // Fulfill
-    } else {
-        // Reject
-    }
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', conclusionPromises);
+
+function conclusionPromises(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = getFormData(event.currentTarget);
+
+  for (let i = 0; i <= amount; i += 1) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  event.currentTarget.reset();
 }
-
-const firstDelay = document.querySelector("input[name=delay]")
-const step = document.querySelector("input[name=step]")
-const amount = document.querySelector("input[name=amount]")
-
-const formEl = document.querySelector(".form")
-
-
 
 function getFormData(el) {
+  const formData = {};
 
-    const formData = {}
-
-    new FormData(el).forEach((value, name) => (formData[name] = Number(value)));
-    console.log(formData);
-    return formData;
-
+  new FormData(el).forEach((value, name) => (formData[name] = Number(value)));
+  return formData;
 }
 
-formEl && formEl.addEventListener("submit", function(event){
-    event.preventDefault()
-    console.log('click');  
-    
-    getFormData(event.currentTarget)
-})
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-//getFormData(formEl)
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
